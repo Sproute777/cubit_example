351b5241344c66b7142894577f08b6eaa78de2c6
Dmitry Bannikov <prestiz4321@gmail.com>
2021-12-20 16:09:01 +0300

    private cubits

--- lib/counter/bloc/sum_bloc.dart
+++ lib/counter/bloc/sum_bloc.dart
@@ -9,14 +9,17 @@ part 'sum_event.dart';
 part 'sum_state.dart';
 
 class SumBloc extends Bloc< SumEvent ,SumState> {
-  SumBloc({ required this.blueCubit, required this.greenCubit}) : super(const SumState()){
+  SumBloc({ required BlueCubit blueCubit, required GreenCubit greenCubit}) : 
+  _blueCubit = blueCubit,
+  _greenCubit = greenCubit,
+  super(const SumState()){
     on<BlueValue>(_onBlueValue);
     on<GreenValue>(_onGreenValue);   
-    _subsBlue = blueCubit.stream.listen((b)=> add(BlueValue(value: b)));
-    _subsGreen = greenCubit.stream.listen((g)=> add(GreenValue(value: g)));   
+    _subsBlue = _blueCubit.stream.listen((b)=> add(BlueValue(value: b)));
+    _subsGreen = _greenCubit.stream.listen((g)=> add(GreenValue(value: g)));   
   }
-   late final   BlueCubit   blueCubit;
-   late final   GreenCubit  greenCubit;
+   late final   BlueCubit   _blueCubit;
+   late final   GreenCubit  _greenCubit;
    late final   StreamSubscription<int>? _subsBlue;
    late final   StreamSubscription<int>? _subsGreen;
 
